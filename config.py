cecb8b61dbc4c4861fda2a60ec86c949dad03e90
ramiro_mv11 <ram.martnez11@gmail.com>
2026-05-13 15:45:09 -0600

    Prueba: Cambios en develop subidos a main
    Se realiza prueba para verificar:
    - Que los cambios se suban a la rama develop
    - Se haga merge en main
    - Se actualice el servidor

--- config.py
+++ config.py
@@ -1,1 +1,16 @@
+import os
 
+class Config:
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MYSQL_HOST = '190.190.0.75'
+    MYSQL_USER = 'geo'
+    MYSQL_PASSWORD = 'N1usuario'
+    MYSQL_DB = 'usuarios_pro_adm'
+    
+config = {
+    'development': DevelopmentConfig
+}
