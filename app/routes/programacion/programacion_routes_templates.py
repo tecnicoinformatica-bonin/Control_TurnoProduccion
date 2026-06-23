44367b4ae62465315606ba599e61a340f44d197b
ramiro_mv11 <ram.martnez11@gmail.com>
2026-06-23 10:01:53 -0600

    feat(frontend): Se mejora vista de programaciones
    Anteriormente la lista de programaciones se encontraba en home.html, ahora se transfiere a programaciones_general.html, para mejorar la modularización de la aplicación web.

--- app/routes/programacion/programacion_routes_templates.py
+++ app/routes/programacion/programacion_routes_templates.py
@@ -1,11 +1,16 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template, abort
-from flask_login import login_required
+from flask_login import current_user, login_required
 
 from app.api.motivo_desasignacion.motivo_desasignacion_service import Motivo_desasignacion_Service
 from app.api.programacion.programacion_service import Programacion_Service
 from app.api.departamento.departamento_service import Departamento_Service
 from app.api.registro.registro_service import Registro_Service
+from app.api.rol.rol_service import Rol_Service
+from app.api.usuario.usuario_repository import UsuarioRepository
 from app.api.usuario.usuario_service import Usuario_Service
+from app.api.usuario_rol.usuario_rol_service import Usuario_Rol_Service
 from app.core.auth.permiso_requerido_decorator import permiso_requerido
 from app.extensions.db import db
 
@@ -71,3 +76,38 @@ def editarProgramacion_template(idDepartment, fecha):
         registros = registros,
         motivos = motivos
     )
+
+@programacion_template_bp.route("/programaciones_general")
+@login_required
+@permiso_requerido("programacion.ver")
+def programaciones_general():
+    departamentos = Departamento_Service.getDepartamentos_service(db)
+    departamentos_aplica_horas_extra = Departamento_Service.getDepartamentos_aplica_horas_extra_service(db)
+    roles = Rol_Service.getRoles_service(db)
+    usuario_roles = Usuario_Rol_Service.getUsuario_Roles_service(db)
+    programaciones_borrador = Programacion_Service.getProgramacionesActivas_service(db)
+    programaciones = Programacion_Service.getProgramaciones_service(db)
+    fecha_actual = datetime.now()
+
+    departamentos_usuario = UsuarioRepository.getUserDepartmentsById(db, current_user.id)
+
+    idDepartment = 0
+    for d in departamentos_usuario:
+        idDepartment = d["idDepartment"]
+
+    dept = 0
+    for d in departamentos_aplica_horas_extra:
+        dept = d['idDepartment']
+    
+    return render_template(
+        "programacion/programaciones_general.html", 
+        departamentos = departamentos,
+        departamentos_aplica_horas_extra = departamentos_aplica_horas_extra,
+        roles = roles,
+        usuario_roles = usuario_roles,
+        fecha_actual = fecha_actual,
+        programaciones_borrador = programaciones_borrador,
+        dept = dept,
+        programaciones = programaciones,
+        idDepartment = idDepartment,
+        )
