a7abd2fff7466892f83915ce24c47dfc29dded36
ramiro_mv11 <ram.martnez11@gmail.com>
2026-07-15 15:39:25 -0600

    feat(backend): Se modifica el decorador permiso_requerido_decorator.py, agregando funcionalidad para varios permisos y validación si necesita todos esos permisos
    Los parámetros de la función decorador son:
    - *permisos, que permite ingresar varios permisos separados por coma (",")
    - require_all=False, que valida si se requiere uno de los permisos o todos los permisos que se coloquen como argumento. El valor por defecto es False.

--- app/core/auth/permiso_requerido_decorator.py
+++ app/core/auth/permiso_requerido_decorator.py
@@ -2,14 +2,25 @@ from functools import wraps
 from flask import abort
 from flask_login import current_user
 
-def permiso_requerido(permiso):
+def permiso_requerido(*permisos, require_all=False):
 
     def decorator(f):
 
         @wraps(f)
         def decorated_function(*args, **kwargs):
 
-            if not current_user.tiene_permiso(permiso):
+            if require_all:
+                autorizado = all(
+                    current_user.tiene_permiso(p)
+                    for p in permisos
+                )
+            else:
+                autorizado = any(
+                    current_user.tiene_permiso(p)
+                    for p in permisos
+                )
+
+            if not autorizado:
                 abort(403)
 
             return f(*args, **kwargs)
