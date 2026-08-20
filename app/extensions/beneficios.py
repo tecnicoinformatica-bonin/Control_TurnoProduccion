426ee79144465bec4cf485750a1e844452bc8b16
ramiro_mv11 <ram.martnez11@gmail.com>
2026-08-20 12:16:17 -0600

    fix(backend): Se agregan personas que entran desde las 10

--- app/extensions/beneficios.py
+++ app/extensions/beneficios.py
@@ -56,7 +56,7 @@ def calcular_beneficios(fecha, hora_inicio, hora_fin):
     # CENA CON COSTO
     # =====================================================
 
-    if 13 <= hora_entrada <= 23.5:
+    if 10 <= hora_entrada <= 23.5:
         cena_con_costo = True
 
     # =====================================================
